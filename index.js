c07fa6d882181ba7a3c7026f7d6e74b6f12384a3
James <kamaujames3705@gmail.com>
2023-03-09 21:04:29 +0300

    used return

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-function introduction(name) {
-    console.log(`Hi, my name is ${name}.`)
+function introduction(name){
+    return`Hi, my name is ${name}.`;
 }
-
-
 
 function introductionWithLanguage(name, language){
-    console.log(`Hi, my name is ${name} and I am learning to program in ${language}`)
+    return`Hi, my name is ${name} and I am learning to program in ${language}`;
 }
-
 
 function introductionWithLanguageOptional(name, language = "JavaScript"){
-    console.log(`Hi, my name is ${name} and I am learning to program in ${language}`)
+    return`Hi, my name is ${name} and I am learning to program in ${language}`;
 }
 
-introduction("James")
-introductionWithLanguage("James", "JavaScript")
-introductionWithLanguageOptional("James")
+introduction("Ian")
+introductionWithLanguage("Ian","JavaScript")
+introductionWithLanguageOptional("Ian")
